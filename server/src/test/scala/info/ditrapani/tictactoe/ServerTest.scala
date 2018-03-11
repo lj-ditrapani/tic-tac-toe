f01fd27f8351e33e1e5a22876508d76c31f1b001
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2018-03-10 20:30:26 -0500

    Server test for index

--- server/src/test/scala/info/ditrapani/tictactoe/ServerTest.scala
+++ server/src/test/scala/info/ditrapani/tictactoe/ServerTest.scala
@@ -1,15 +1,29 @@
 package info.ditrapani.tictactoe
 
 import cats.effect.IO
-import org.http4s.{HttpService, Method, Request, Uri}
+import org.http4s.{HttpService, Method, Request, Status, Uri}
 import org.http4s.syntax.KleisliSyntax
+import org.http4s.util.CaseInsensitiveString
+import org.scalatest.OptionValues
+import state.game
+import state.Player1
+import scala.io.Source
 
 @SuppressWarnings(Array("org.wartremover.warts.Throw"))
-class ServerTest extends Spec with KleisliSyntax {
-  val service: HttpService[IO] = Server.service
+class ServerTest extends Spec with KleisliSyntax with OptionValues {
   "get index" in {
+    val state = ServerState(1, 2, game.Init, Player1)
+    val service: HttpService[IO] = new Server(state).service
     val getRoot = Request[IO](Method.GET, Uri.uri("/"))
-    service.orNotFound.run(getRoot).unsafeRunSync shouldBe false
-    // test that cookie is set with correct p1 value
+    val response = service.orNotFound.run(getRoot).unsafeRunSync
+    response.status shouldBe Status.Ok
+    val headers = response.headers
+    val cookie = headers.get(CaseInsensitiveString("Set-Cookie")).value.value
+    cookie shouldBe "id=1"
+    val contentType = headers.get(CaseInsensitiveString("Content-Type")).value.value
+    contentType shouldBe "text/html"
+    val body = response.body.map(_.toChar).compile.toVector.unsafeRunSync.mkString("")
+    val index: String = Source.fromResource("index.html").mkString
+    body shouldBe index
   }
 }
