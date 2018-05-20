e9142dda439c7dbb73de14921a95db92483bcf67
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2018-05-19 22:49:15 -0400

    Update server tests

--- server/src/test/scala/info/ditrapani/tictactoe/ServerTest.scala
+++ server/src/test/scala/info/ditrapani/tictactoe/ServerTest.scala
@@ -1,19 +1,29 @@
 package info.ditrapani.tictactoe
 
 import cats.effect.IO
+import fs2.async.Ref
 import org.http4s.{HttpService, Method, Request, Status, Uri}
 import org.http4s.syntax.KleisliSyntax
 import org.http4s.util.CaseInsensitiveString
 import org.scalatest.OptionValues
 import state.game
-import state.{Board, Player1}
+import game.Game
+import state.{Board, Player, Player1}
 import scala.io.Source
 
 @SuppressWarnings(Array("org.wartremover.warts.Throw"))
 class ServerTest extends Spec with KleisliSyntax with OptionValues {
+  def makeServerState(gameState: game.Game, player: Player): ServerState = {
+    val io = for {
+      gameStateRef <- Ref[IO, Game](gameState)
+      playerRef <- Ref[IO, Player](player)
+    } yield ServerState(1, 2, gameStateRef, playerRef)
+    io.unsafeRunSync
+  }
+
   "get index" - {
     "when game.Init" in {
-      val state = ServerState(1, 2, game.Init, Player1)
+      val state = makeServerState(game.Init, Player1)
       val server = new Server(state)
       val service: HttpService[IO] = server.service
       val getRoot = Request[IO](Method.GET, Uri.uri("/"))
@@ -27,12 +37,12 @@ class ServerTest extends Spec with KleisliSyntax with OptionValues {
       val body = response.body.map(_.toChar).compile.toVector.unsafeRunSync.mkString("")
       val index: String = Source.fromResource("index.html").mkString
       body shouldBe index
-      server.gameState shouldBe game.Player1Ready
+      server.gameStateRef.get.unsafeRunSync shouldBe game.Player1Ready
     }
 
     "when game.Player1Ready" - {
       "and player's cookie id != player1 id; sets cookie to Player2 & advances state" in {
-        val state = ServerState(1, 2, game.Player1Ready, Player1)
+        val state = makeServerState(game.Player1Ready, Player1)
         val server = new Server(state)
         val service: HttpService[IO] = server.service
         val getRoot = Request[IO](Method.GET, Uri.uri("/"))
@@ -46,11 +56,11 @@ class ServerTest extends Spec with KleisliSyntax with OptionValues {
         val body = response.body.map(_.toChar).compile.toVector.unsafeRunSync.mkString("")
         val index: String = Source.fromResource("index.html").mkString
         body shouldBe index
-        server.gameState shouldBe game.Player1Turn(Board.init)
+        server.gameStateRef.get.unsafeRunSync shouldBe game.Player1Turn(Board.init)
       }
 
       "and player's cookie id == player1 id; does not set cookie again or advance state" ignore {
-        val state = ServerState(1, 2, game.Player1Ready, Player1)
+        val state = makeServerState(game.Player1Ready, Player1)
         val server = new Server(state)
         val service: HttpService[IO] = server.service
         val getRoot = Request[IO](Method.GET, Uri.uri("/")).addCookie("id", "1")
@@ -64,12 +74,12 @@ class ServerTest extends Spec with KleisliSyntax with OptionValues {
         val body = response.body.map(_.toChar).compile.toVector.unsafeRunSync.mkString("")
         val index: String = Source.fromResource("index.html").mkString
         body shouldBe index
-        server.gameState shouldBe game.Player1Ready
+        server.gameStateRef.get.unsafeRunSync shouldBe game.Player1Ready
       }
     }
 
     "when any other game state" in {
-      val state = ServerState(1, 2, game.Player2Ready, Player1)
+      val state = makeServerState(game.Player2Ready, Player1)
       val server = new Server(state)
       val service: HttpService[IO] = server.service
       val getRoot = Request[IO](Method.GET, Uri.uri("/"))
@@ -83,7 +93,7 @@ class ServerTest extends Spec with KleisliSyntax with OptionValues {
       val body = response.body.map(_.toChar).compile.toVector.unsafeRunSync.mkString("")
       val index: String = Source.fromResource("index.html").mkString
       body shouldBe index
-      server.gameState shouldBe game.Player2Ready
+      server.gameStateRef.get.unsafeRunSync shouldBe game.Player2Ready
     }
   }
 }
