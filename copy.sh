269ec1775c323118c583a36891e5d8b59c6584c2
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2018-05-21 11:53:45 -0400

    Add source map

--- copy.sh
+++ copy.sh
@@ -1,1 +1,1 @@
-cp client/target/scala-2.12/{client-jsdeps.js,client-opt.js} server/src/main/resources/js
+cp client/target/scala-2.12/{client-jsdeps.js,client-opt.js,client-opt.js.map} server/src/main/resources/js
