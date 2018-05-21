dce7740ad8eae854d337a24ac3109d7c66f8826c
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2018-05-20 21:50:34 -0400

    Shared code: Complete Game tests
    Now all shared code is 100% tested

--- shared/src/test/scala/info/ditrapani/tictactoe/state/game/GameTest.scala
+++ shared/src/test/scala/info/ditrapani/tictactoe/state/game/GameTest.scala
@@ -8,6 +8,11 @@ class GameTest extends Spec {
   private val boardT2 = "2T2EEEXXXEEE"
   private val boardG1 = "2G1OOOEEEEEE"
   private val boardG2 = "1G2EEEEEEXXX"
+  private val boardGT = "2GTXXOOOXXXO"
+  private val boardX1 = "2X1XXOOOXXXO"
+  private val boardX2 = "2X2XXOOOXXXO"
+  private val boardQ1 = "2Q1XXOOOXXXO"
+  private val boardQ2 = "2Q2XXOOOXXXO"
 
   "fromStatusString" - {
     val tests = List(
@@ -17,13 +22,20 @@ class GameTest extends Spec {
       (boardT1, Turn(Player1, Board.fromStatusString(boardT1))),
       (boardT2, Turn(Player2, Board.fromStatusString(boardT2))),
       (boardG1, GameOver(P1Wins, Board.fromStatusString(boardG1))),
-      (boardG2, GameOver(P2Wins, Board.fromStatusString(boardG2)))
+      (boardG2, GameOver(P2Wins, Board.fromStatusString(boardG2))),
+      (boardGT, GameOver(Tie, Board.fromStatusString(boardGT))),
+      (boardX1, Reset(Player1, Board.fromStatusString(boardX1))),
+      (boardX2, Reset(Player2, Board.fromStatusString(boardX2))),
+      (boardQ1, Quit(Player1, Board.fromStatusString(boardQ1))),
+      (boardQ2, Quit(Player2, Board.fromStatusString(boardQ2))),
     )
+
     for ((status, game) <- tests) {
       s"given $status returns $game" in {
         Game.fromStatusString(status) shouldBe game
       }
     }
+
     "throws an IllegalArgumentException if status string is too short" in {
       an[IllegalArgumentException] should be thrownBy { Game.fromStatusString("1R2EEEAAAEE") }
     }
@@ -45,8 +57,13 @@ class GameTest extends Spec {
       (Turn(Player1, Board.fromStatusString(boardT1)), "T1XXXOOOXXX"),
       (Turn(Player2, Board.fromStatusString(boardT2)), "T2EEEXXXEEE"),
       (GameOver(P1Wins, Board.fromStatusString(boardG1)), "G1OOOEEEEEE"),
-      (GameOver(P2Wins, Board.fromStatusString(boardG2)), "G2EEEEEEXXX")
+      (GameOver(P2Wins, Board.fromStatusString(boardG2)), "G2EEEEEEXXX"),
+      (Reset(Player1, Board.fromStatusString(boardX1)), "X1XXOOOXXXO"),
+      (Reset(Player2, Board.fromStatusString(boardX1)), "X2XXOOOXXXO"),
+      (Quit(Player1, Board.fromStatusString(boardQ2)), "Q1XXOOOXXXO"),
+      (Quit(Player2, Board.fromStatusString(boardQ2)), "Q2XXOOOXXXO"),
     )
+
     for ((game, status) <- tests) {
       s"given $game returns $status" in {
         game.toResponse shouldBe status
@@ -55,6 +72,8 @@ class GameTest extends Spec {
   }
 
   "toMessage" - {
+    val rematchMesssage = "Player 1 wants a rematch.  Do you accept?"
+
     val tests = List(
       (Init, Actor.player1, "No players have joined yet..."),
       (Ready(Player1), Actor.player1, "Waiting for Player 2 to join."),
@@ -64,8 +83,11 @@ class GameTest extends Spec {
       (Turn(Player2, Board.fromStatusString(boardT2)), Actor.player1, "Player 2's turn"),
       (GameOver(P1Wins, Board.fromStatusString(boardG1)), Spectator, "Player 1 wins!"),
       (GameOver(P1Wins, Board.fromStatusString(boardG1)), Actor.player1, "You win!"),
-      (GameOver(P2Wins, Board.fromStatusString(boardG2)), Actor.player1, "You loose :(")
+      (GameOver(P2Wins, Board.fromStatusString(boardG2)), Actor.player1, "You loose :("),
+      (Reset(Player1, Board.fromStatusString(boardX1)), Actor.player2, rematchMesssage),
+      (Quit(Player2, Board.fromStatusString(boardQ2)), Actor.player2, "You quit...quitter"),
     )
+
     for ((game, player, message) <- tests) {
       s"given player: $player and $game returns $message" in {
         game.toMessage(player) shouldBe message
